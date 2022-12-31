db74c3c36df9437a3182894a446a35a55ce9a35c
Piotr Wodziński <piotr@wodzinski.org>
2022-12-31 18:11:38 +0100

    Initial Commit

---
+++ Smart Home Info/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Smart Home Info
+//
+//  Created by Piotr Wodziński on 31/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Smart Home Info/Smart_Home_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Home_InfoApp.swift
+//  Smart Home Info
+//
+//  Created by Piotr Wodziński on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Home_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
